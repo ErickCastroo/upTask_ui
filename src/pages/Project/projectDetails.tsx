34e31aac3eed120de943dbd541bbbfb53193c42a
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-04-24 11:44:38 -0700

    Add AddTaskModal component and implement ProjectDetails page with project fetching and loading states

---
+++ src/pages/Project/projectDetails.tsx
@@ -0,0 +1,42 @@
+import { useNavigate, useParams } from 'react-router-dom'
+import { toast } from 'react-toastify'
+
+import { useQuery } from '@tanstack/react-query'
+import { GetProjectById } from '@/api/project'
+import { IsLoading } from '@/components/isLoading'
+
+function ProjectDetails() {
+  const params = useParams()
+  const usenavigate = useNavigate()
+  const projectId = params.projectId!
+
+  const { data, isError, isLoading } = useQuery({
+    queryKey: ['ProjectDetails', projectId],
+    queryFn: () => GetProjectById(projectId),
+    retry: false,
+  })
+
+  if (isLoading) return <IsLoading />
+  if (isError) {
+    toast.error('Error al cargar el proyecto')
+    usenavigate('/')
+  }
+
+  return (
+    <>
+      <h1 className='text-5xl font-black'>{data.projectName}</h1>
+      <p className='text-xl font-light text-purple-500 mt-5'>{data.description}</p>
+
+      <nav className='my-5 flex gap-2'>
+        <button
+          className='bg-indigo-600 text-white px-10 py-3 rounded-lg inline-block hover:bg-indigo-800 transition-colors font-semibold'
+          onClick={() => usenavigate(`?newTask=true`)}
+        >
+          Agregar Tarea
+        </button> 
+      </nav>
+    </>
+  )
+}
+
+export { ProjectDetails }
