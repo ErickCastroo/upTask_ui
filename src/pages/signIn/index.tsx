892bc81daf13b7e68d250f79e5e2837aa6972278
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-05-28 13:37:06 -0700

    Añadir página de registro; implementar formulario con campos de usuario, email, contraseña y confirmación de contraseña.

--- src/pages/signIn/index.tsx
+++ src/pages/signIn/index.tsx
@@ -1,50 +1,49 @@
-import { Link } from "react-router-dom"
+import { Link } from 'react-router-dom'
 
 function Login() {
   return (
     <div className="relative min-h-screen bg-cover bg-center bg-[url('./bgAuth.jpg')]">
-      <div className="absolute inset-0 bg-black/70" />
+      <div className='absolute inset-0 bg-black/80' />
 
-      <div className="relative z-10 flex items-center justify-center min-h-screen">
-        <div className="bg-black/75 backdrop-blur-md w-1/2 xl:w-1/3 p-8 rounded-xs shadow-lg">
-          <h1 className="text-4xl font-bold text-center mb-4 text-white">
-            Login
+      <div className='relative z-10 flex items-center justify-center min-h-screen'>
+        <div className='bg-black/75 backdrop-blur-md w-4/5 xl:w-1/3 p-8 rounded-xs shadow-lg'>
+          <h1 className='text-4xl font-bold text-center mb-4 text-white'>
+            Sign In
           </h1>
-          <p className="text-purple-300 flex justify-center">Create you Account
-            <Link to="/signUp" className="text-purple-500 hover:text-purple-700 ml-2">
+          <p className='text-purple-300 mb-1 md:flex md:justify-center'>Create you Account
+            <Link to='/signUp' className='text-purple-500 hover:text-purple-700 ml-2'>
               Sign Up
             </Link>
           </p>
-          <form className="space-y-4">
+          <form className='space-y-4'>
+
             <div>
-              <label className="block text-white mb-2" htmlFor="email">Email</label>
+              <label className='block text-white mb-2' htmlFor='email'>Email</label>
               <input
-                type="email"
-                id="email"
-                className="w-full p-2  border-b-1 border-purple-300 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
-                placeholder="Enter your email"
+                type='email'
+                id='email'
+                className='w-full py-2 border-b-1 border-purple-300 text-white focus:outline-none focus:ring-2 focus:ring-purple-500'
+                placeholder='Enter your email'
                 required
               />
             </div>
             <div>
-              <label className="block text-white mb-2" htmlFor="password">Password</label>
+              <label className='block text-white mb-2' htmlFor='password'>Password</label>
               <input
-                type="password"
-                id="password"
-                className="w-full p-2 border-b-1 border-purple-300 text-white focus:outline-none focus:ring-2 focus:ring-purple-500"
-                placeholder="Enter your password"
+                type='password'
+                id='password'
+                className='w-full py-2 border-b-1 border-purple-300 text-white focus:outline-none focus:ring-2 focus:ring-purple-500'
+                placeholder='Enter your password'
                 required
               />
             </div>
             <button
-              type="submit"
-              className=" w-1/3 bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-200"
+              type='submit'
+              className=' md:w-1/3 w-full rounded-md bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4  focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-200'
             >
-              Login
+              Sign In
             </button>
-
           </form>
-
         </div>
       </div>
     </div>
--- src/pages/signUp/index.tsx
+++ src/pages/signUp/index.tsx
@@ -1,7 +1,73 @@
+import { Link } from 'react-router-dom'
 function Register() {
   return (
-    <div>
-      <h1>Register</h1>
+    <div className="relative min-h-screen bg-cover bg-center bg-[url('./bgAuth.jpg')]">
+      <div className='absolute inset-0 bg-black/80' />
+
+      <div className='relative z-10 flex items-center justify-center min-h-screen'>
+        <div className='bg-black/75 backdrop-blur-md w-1/2 xl:w-1/3 p-8 rounded-xs shadow-lg'>
+          <h1 className='text-4xl font-bold text-center mb-4 text-white'>
+            Sign Up
+          </h1>
+          <p className='text-purple-300 flex justify-center'>Already have an account?
+            <Link to='/signIn' className='text-purple-500 hover:text-purple-700 ml-2'>
+              Sign In
+            </Link>
+          </p>
+          <form className='space-y-4'>
+
+            <div>
+              <label className='block text-white mb-2' htmlFor='email'>Username</label>
+              <input
+                type='text'
+                id='email'
+                className='w-full py-2 border-b-1 border-purple-300 text-white focus:outline-none focus:ring-2 focus:ring-purple-500'
+                placeholder='Enter your username'
+                required
+              />
+            </div>
+
+            <div>
+              <label className='block text-white mb-2' htmlFor='email'>Email</label>
+              <input
+                type='email'
+                id='email'
+                className='w-full py-2 border-b-1 border-purple-300 text-white focus:outline-none focus:ring-2 focus:ring-purple-500'
+                placeholder='Enter your email'
+                required
+              />
+            </div>
+            <div>
+              <label className='block text-white mb-2' htmlFor='password'>Password</label>
+              <input
+                type='password'
+                id='password'
+                className='w-full py-2 border-b-1 border-purple-300 text-white focus:outline-none focus:ring-2 focus:ring-purple-500'
+                placeholder='Enter your password'
+                required
+              />
+            </div>
+            <div>
+              <label className='block text-white mb-2' htmlFor='password'>Confirm Password</label>
+              <input
+                type='password'
+                id='confirmpassword'
+                className='w-full py-2 border-b-1 border-purple-300 text-white focus:outline-none focus:ring-2 focus:ring-purple-500'
+                placeholder='Confirm your password'
+                required
+              />
+            </div>
+            <button
+              type='submit'
+              className='md:w-1/3 w-full rounded-md bg-purple-600 hover:bg-purple-700 text-white font-bold py-2 px-4  focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-200'
+            >
+              Sign Up
+            </button>
+
+          </form>
+
+        </div>
+      </div>
     </div>
   )
 }
