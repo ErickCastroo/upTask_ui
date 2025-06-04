a835d68bb71d6e8ca3e3135ae8da4df6f6dba7da
ErickCastroo <castrocamachoerickmiguel21@gmail.com>
2025-06-04 09:10:48 -0700

    feat: agregar interceptor de solicitud para incluir token de autorización en las cabeceras

--- src/libs/axios/index.ts
+++ src/libs/axios/index.ts
@@ -4,4 +4,14 @@ const Api = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
 })
 
+Api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('tokenUptask')
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`
+    }
+    return config
+  }
+)
+
 export { Api }
